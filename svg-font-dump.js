fa5216b06752750390323dffe409dc35380fb36b
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-05-03 20:13:49 +0400

    fixed multibyte unicode support (needs fixed xmldom)

--- svg-font-dump.js
+++ svg-font-dump.js
@@ -61,6 +61,39 @@ var args = parser.parseArgs();
 
 ////////////////////////////////////////////////////////////////////////////////
 
+// Int to char, with fix for big numbers
+// see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String/fromCharCode
+//
+function fixedFromCharCode(code) {
+  /*jshint bitwise: false*/
+  if (code > 0xffff) {
+    code -= 0x10000;
+
+    var surrogate1 = 0xd800 + (code >> 10)
+      , surrogate2 = 0xdc00 + (code & 0x3ff);
+
+    return String.fromCharCode(surrogate1, surrogate2);
+  } else {
+    return String.fromCharCode(code);
+  }
+}
+// Char to Int, with fix for big numbers
+//
+function fixedCharCodeAt(chr) {
+  /*jshint bitwise: false*/
+  var char1 = chr.charCodeAt(0)
+    , char2 = chr.charCodeAt(1);
+
+  if ((chr.length >= 2) &&
+      ((char1 & 0xfc00) === 0xd800) &&
+      ((char2 & 0xfc00) === 0xdc00)) {
+    return 0x10000 + ((char1 - 0xd800) << 10) + (char2 - 0xdc00);
+  } else {
+    return char1;
+  }
+}
+
+
 function font_dump(data) {
 
   var result = []
@@ -90,8 +123,6 @@ function font_dump(data) {
     var d = glyph.getAttribute('d');
     
     var unicode = glyph.getAttribute('unicode');
-
-    unicode = unicode.length === 1 ? unicode.charCodeAt(0) : unicode;
 
     var name = glyph.getAttribute('glyph-name') || ('glyph' + unicode);
 
@@ -153,6 +184,9 @@ glyphs.forEach(function(glyph) {
 
   var exists,
       glyph_out = {};
+
+  // Convert multibyte unicode char to number
+  glyph.unicode = fixedCharCodeAt(glyph.unicode);
 
   // if got config from existing font, then write only missed files
   if (config) {
