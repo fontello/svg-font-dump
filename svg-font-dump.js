9247fc0bcd68cc0f173ddd56949a12b55015f5fd
Vitaly Puzrin <vitaly@rcdesign.ru>
2013-05-06 23:29:08 +0400

    improved file names for unknown glyphs & removed scale(1)

--- svg-font-dump.js
+++ svg-font-dump.js
@@ -22,39 +22,12 @@ var parser = new ArgumentParser({
   addHelp: true,
   description: 'Dump SVG font to separate glyphs'
 });
-parser.addArgument(
-  [ '-c', '--config' ],
-  {
-    help: 'Font config file'
-  }
-);
-parser.addArgument(
-  [ '-i', '--src_font' ],
-  {
-    help: 'Source font path',
-    required: true
-  }
-);
-parser.addArgument(
-  [ '-o', '--glyphs_dir' ],
-  {
-    help: 'Glyphs output folder',
-    required: true
-  }
-);
-parser.addArgument(
-  [ '-d', '--diff_config' ],
-  {
-    help: 'Difference config output file'
-  }
-);
-parser.addArgument(
-  [ '-f', '--force' ],
-  {
-    help: 'Force override glyphs from config',
-    action: 'storeTrue'
-  }
-);
+parser.addArgument([ '-c', '--config' ], { help: 'Font config file' });
+parser.addArgument([ '-i', '--src_font' ], { help: 'Source font path', required: true });
+parser.addArgument([ '-o', '--glyphs_dir' ], { help: 'Glyphs output folder', required: true });
+parser.addArgument([ '-d', '--diff_config' ], { help: 'Difference config output file' });
+parser.addArgument([ '-f', '--force' ], { help: 'Force override glyphs from config', action: 'storeTrue'});
+parser.addArgument([ '-n', '--names' ], { help: 'Try to guess new glyphs names', action: 'storeTrue'});
 
 var args = parser.parseArgs();
 
@@ -64,19 +37,19 @@ var args = parser.parseArgs();
 // Int to char, with fix for big numbers
 // see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String/fromCharCode
 //
-function fixedFromCharCode(code) {
-  /*jshint bitwise: false*/
-  if (code > 0xffff) {
-    code -= 0x10000;
-
-    var surrogate1 = 0xd800 + (code >> 10)
-      , surrogate2 = 0xdc00 + (code & 0x3ff);
-
-    return String.fromCharCode(surrogate1, surrogate2);
-  } else {
-    return String.fromCharCode(code);
-  }
-}
+//function fixedFromCharCode(code) {
+//  /*jshint bitwise: false*/
+//  if (code > 0xffff) {
+//    code -= 0x10000;
+//
+//    var surrogate1 = 0xd800 + (code >> 10)
+//      , surrogate2 = 0xdc00 + (code & 0x3ff);
+//
+//    return String.fromCharCode(surrogate1, surrogate2);
+//  } else {
+//    return String.fromCharCode(code);
+//  }
+//}
 // Char to Int, with fix for big numbers
 //
 function fixedCharCodeAt(chr) {
@@ -115,13 +88,13 @@ function font_dump(data) {
   descent       = -fontFace.getAttribute('descent');
 
   _.each(font.getElementsByTagName('glyph'), function(glyph) {
-    
+
     // Ignore empty glyphs (with empty code or path)
     if (!glyph.hasAttribute('d')) { return; }
     if (!glyph.hasAttribute('unicode')) { return; }
-    
+
     var d = glyph.getAttribute('d');
-    
+
     var unicode = glyph.getAttribute('unicode');
 
     var name = glyph.getAttribute('glyph-name') || ('glyph' + unicode);
@@ -137,14 +110,17 @@ function font_dump(data) {
 
     // vertical mirror
     var transform = 'translate(0 ' + (glyphSize / 2) + ') scale(1 -1) translate(0 ' + (-glyphSize / 2) + ')';
-    // scale
-    transform += ' scale(' + scale + ')';
+
+    if (scale !== 1) {
+      // scale size, only when needed
+      transform += ' scale(' + scale + ')';
+      // recalculate width & height
+      width = width * scale;
+      height = height * scale;
+    }
     // descent shift
     transform += ' translate(0 ' + descent + ')';
 
-    // recalculate width & height
-    width = width * scale;
-    height = height * scale;
 
     result.push({
       d: d,
@@ -230,7 +206,19 @@ glyphs.forEach(function(glyph) {
 
   console.log((glyph.unicode.toString(16)) + ' - NEW glyph, writing...');
 
-  fs.writeFile(path.join(args.glyphs_dir, glyph.name + '.svg'), glyph.svg);
+  var filename;
+
+  if (args.names) {
+    filename = glyph.name + '.svg';
+  } else {
+    if (glyph.unicode === +glyph.unicode) {
+      filename = 'glyph__' + glyph.unicode.toString(16) + '.svg';
+    } else {
+      filename = 'glyph__' + glyph.unicode + '.svg';
+    }
+  }
+
+  fs.writeFile(path.join(args.glyphs_dir, filename), glyph.svg);
 
   diff.push(glyph_out);
 
