60616bfc973d62e0610bcc2cbf9106c8f745a035
Renārs Vilnis <renars.vilnis@gmail.com>
2016-12-01 19:50:54 +0100

    Seperate util functions into seperate files and convert to moder (>5) node

--- svg-font-dump.js
+++ svg-font-dump.js
@@ -7,9 +7,11 @@ var path = require('path');
 var crypto = require('crypto');
 var _ = require('lodash');
 var yaml = require('js-yaml');
-var SvgPath = require('svgpath');
-var XMLDOMParser = require('xmldom').DOMParser;
 var ArgumentParser = require('argparse').ArgumentParser;
+
+const fixedCharCodeAt = require('./lib/utils/fixedCharCodeAt');
+const loadSvgData = require('./lib/utils/loadSvgData');
+const loadFontelloData = require('./lib/utils/loadFontelloData');
 
 var svgTemplate = _.template(
     '<svg height="<%= height %>" width="<%= width %>" xmlns="http://www.w3.org/2000/svg">' +
@@ -30,123 +32,6 @@ parser.addArgument([ '-f', '--force' ], { help: 'Force override glyphs from conf
 parser.addArgument([ '-n', '--names' ], { help: 'Try to guess new glyphs names', action: 'storeTrue' });
 
 var args = parser.parseArgs();
-
-// //////////////////////////////////////////////////////////////////////////////
-
-// Int to char, with fix for big numbers
-// see https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/String/fromCharCode
-//
-function fixedFromCharCode (code) {
-  /* jshint bitwise: false */
-  if (code > 0xffff) {
-    code -= 0x10000;
-
-    var surrogate1 = 0xd800 + (code >> 10);
-    var surrogate2 = 0xdc00 + (code & 0x3ff);
-
-    return String.fromCharCode(surrogate1, surrogate2);
-  } else {
-    return String.fromCharCode(code);
-  }
-}
-// Char to Int, with fix for big numbers
-//
-function fixedCharCodeAt (chr) {
-  /* jshint bitwise: false */
-  var char1 = chr.charCodeAt(0);
-  var char2 = chr.charCodeAt(1);
-
-  if ((chr.length >= 2) &&
-      ((char1 & 0xfc00) === 0xd800) &&
-      ((char2 & 0xfc00) === 0xdc00)) {
-    return 0x10000 + ((char1 - 0xd800) << 10) + (char2 - 0xdc00);
-  } else {
-    return char1;
-  }
-}
-
-// Load glyphs data from SVG font
-//
-function loadSvgData (data) {
-  var result = [];
-
-  var xmlDoc = (new XMLDOMParser()).parseFromString(data, 'application/xml');
-
-  var svgFont = xmlDoc.getElementsByTagName('font')[0];
-  var svgFontface = xmlDoc.getElementsByTagName('font-face')[0];
-  var svgGlyps = xmlDoc.getElementsByTagName('glyph');
-
-  var fontHorizAdvX = svgFont.getAttribute('horiz-adv-x');
-  var fontAscent = svgFontface.getAttribute('ascent');
-  var fontUnitsPerEm = svgFontface.getAttribute('units-per-em') || 1000;
-
-  var scale = 1000 / fontUnitsPerEm;
-
-  _.each(svgGlyps, function (svgGlyph) {
-    var d = svgGlyph.getAttribute('d');
-
-    // FIXME
-    // Now just ignore glyphs without image, however
-    // that can be space. Does anyone needs it?
-    if (!d) { return; }
-
-    var unicode = svgGlyph.getAttribute('unicode');
-    var name = svgGlyph.getAttribute('glyph-name') || ('glyph' + unicode);
-    var width = svgGlyph.getAttribute('horiz-adv-x') || fontHorizAdvX;
-
-    result.push({
-      d: new SvgPath(d)
-              .translate(0, -fontAscent)
-              .scale(scale, -scale)
-              .abs()
-              .round(1)
-              .rel()
-              .round(1)
-              .toString(),
-
-      unicode: unicode,
-      name: name,
-      width: (width * scale).toFixed(1),
-      height: 1000
-    });
-  });
-
-  return result;
-}
-
-// Load glyphs data from fontello's config (custom icons only)
-//
-function loadFontelloData (data) {
-  var result = [];
-
-  _.each(data.glyphs, function (glyph) {
-    // FIXME
-    // Now just ignore glyphs without image, however
-    // that can be space. Does anyone needs it?
-    if (!(glyph.svg && glyph.svg.path)) { return; }
-
-    result.push({
-      // d: glyph.svg.path,
-      d: new SvgPath(glyph.svg.path)
-              .abs()
-              .round(1)
-              .rel()
-              .round(1)
-              .toString(),
-      width: glyph.svg.width.toFixed(1),
-      height: 1000,
-
-      uid: glyph.uid,
-      unicode: fixedFromCharCode(glyph.code),
-      name: glyph.css || ('glyph' + glyph.code),
-      search: glyph.search || []
-    });
-  });
-
-  return result;
-}
-
-// //////////////////////////////////////////////////////////////////////////////
 
 var data = null;
 var config = null;
